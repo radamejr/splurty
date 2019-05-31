16266660d5368b3daf161d1a003e9bc0489c53bb
radamejr <radamejr@gmail.com>
2019-05-31 02:41:17 +0000

    Added submission to create form, now adds quote to the database

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,4 +7,17 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 
+  def create
+      Quote.create(quote_params)
+      redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+
+
+
 end
