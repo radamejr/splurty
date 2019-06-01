72b86a14e4b979d61974052159bd7fc555e92abf
radamejr <radamejr@gmail.com>
2019-06-01 21:40:31 +0000

    Adjusted quote.rb

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,4 @@
-class Quote < ApplicationRecord
+class Quote < ApplicationRecord::Base
   validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
   validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 end
